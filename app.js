4164eca739ca084592b896111101b13e57e5db6e
Alejandro Viveros <alejandroviverosm@gmail.com>
2021-06-21 21:54:19 -0400

    curso hasta clase numero 10

---
+++ app.js
@@ -0,0 +1,24 @@
+
+
+
+
+var c = {
+
+	nombre: "Carito",
+	apellido: "Vejar",
+	edad: "17"
+};
+
+var d = c;
+
+console.log("caritoC: ", c );
+console.log("caritoD: ", d);
+
+c.apellido= "Calluan";
+
+console.log("caritoC: ", c );
+console.log("caritoD: ", d);
+
+
+
+
